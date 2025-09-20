75463320fa01f03c4e4a6405e996ce8070aada38
Eyefyre <32098546+Eyefyre@users.noreply.github.com>
2025-09-20 18:15:08 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,6 @@
 import json
 from datetime import datetime    
 import requests
-import ast
 
 file = open('connections.json')
 connections = json.load(file)
@@ -11,32 +10,27 @@ file.close()
 id = connections[-1]["id"] + 1
 con_date = datetime.today().strftime('%Y-%m-%d')
 if connections[-1]["date"] == con_date:
-	print(f"Connection #{id-1} from {con_date} already exists in file, exiting")
-	exit()
+    print(f"Connection #{id-1} from {con_date} already exists in file, exiting")
+    exit()
 
 URL = f"https://www.nytimes.com/svc/connections/v2/{con_date}.json" 
 r = requests.get(URL)
 print(r.content)
-try:
-  content = json.loads(r.content)
-except JSONDecodeError as e:
-  print(e)
-except:
-
+content = json.loads(r.content)
 print(content)
 print(f"Adding Connection #{id} from {con_date}")
 groups = []
 for group in content["categories"]:
-  categ = {"level":-1,"group":group["title"],"members":[]}
+    categ = {"level":-1,"group":group["title"],"members":[]}
     for member in group["cards"]:
-      categ["members"].append(member["content"])
-  groups.append(categ)
+        categ["members"].append(member["content"])
+    groups.append(categ)
 
     
 con_item = {"id":int(id),"date":con_date,"answers": groups}
 print(con_item)
 connections.append(con_item)
+    
 with open('connections.json', 'w') as f:
-	json.dump(connections, f, indent=4)
-	f.close()
-
+        json.dump(connections, f, indent=4)
+        f.close()
