605a85edbb6725c9a523e6f8732774c57ed33db4
Eyefyre <32098546+Eyefyre@users.noreply.github.com>
2025-09-20 18:02:34 +0100

    Add files via upload

--- update.py
+++ update.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime    
 import requests
+import ast
 
 file = open('connections.json')
 connections = json.load(file)
@@ -9,14 +10,15 @@ file.close()
 
 id = connections[-1]["id"] + 1
 con_date = datetime.today().strftime('%Y-%m-%d')
-if connections[-1]["date"] == con_date:
-    print(f"Connection #{id-1} from {con_date} already exists in file, exiting")
-    exit()
+# if connections[-1]["date"] == con_date:
+#     print(f"Connection #{id-1} from {con_date} already exists in file, exiting")
+#     exit()
 
 URL = f"https://www.nytimes.com/svc/connections/v2/{con_date}.json" 
 r = requests.get(URL)
-
+print(r.content)
 content = json.loads(r.content)
+print(content)
 print(f"Adding Connection #{id} from {con_date}")
 groups = []
 for group in content["categories"]:
@@ -28,8 +30,8 @@ for group in content["categories"]:
     
 con_item = {"id":int(id),"date":con_date,"answers": groups}
 print(con_item)
-connections.append(con_item)
+# connections.append(con_item)
     
-with open('connections.json', 'w') as f:
-        json.dump(connections, f, indent=4)
-        f.close()
+# with open('connections.json', 'w') as f:
+#         json.dump(connections, f, indent=4)
+#         f.close()
