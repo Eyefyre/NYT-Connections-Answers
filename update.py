bd77a30c2c396c62ed88d72a4d35678cd985d3b8
Eyefyre <32098546+Eyefyre@users.noreply.github.com>
2025-09-20 18:13:42 +0100

    Update update.py

--- update.py
+++ update.py
@@ -10,9 +10,9 @@ file.close()
 
 id = connections[-1]["id"] + 1
 con_date = datetime.today().strftime('%Y-%m-%d')
-# if connections[-1]["date"] == con_date:
-#     print(f"Connection #{id-1} from {con_date} already exists in file, exiting")
-#     exit()
+if connections[-1]["date"] == con_date:
+	print(f"Connection #{id-1} from {con_date} already exists in file, exiting")
+	exit()
 
 URL = f"https://www.nytimes.com/svc/connections/v2/{con_date}.json" 
 r = requests.get(URL)
@@ -27,17 +27,16 @@ print(content)
 print(f"Adding Connection #{id} from {con_date}")
 groups = []
 for group in content["categories"]:
-    categ = {"level":-1,"group":group["title"],"members":[]}
+  categ = {"level":-1,"group":group["title"],"members":[]}
     for member in group["cards"]:
-        categ["members"].append(member["content"])
-    groups.append(categ)
+      categ["members"].append(member["content"])
+  groups.append(categ)
 
     
 con_item = {"id":int(id),"date":con_date,"answers": groups}
 print(con_item)
-# connections.append(con_item)
-    
-# with open('connections.json', 'w') as f:
-#         json.dump(connections, f, indent=4)
+connections.append(con_item)
+with open('connections.json', 'w') as f:
+	json.dump(connections, f, indent=4)
+	f.close()
 
-#         f.close()
